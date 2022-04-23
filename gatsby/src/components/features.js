8ea108d62ad3af3ccb0ebf5d9dc2490648c8b111
Crystal Pederson <crystalpederson88@gmail.com>
2022-04-23 11:11:07 -0700

    add docker feature section

---
+++ gatsby/src/components/features.js
@@ -0,0 +1,52 @@
+import React from "react";
+import dockerLogo from '../assets/docker-logo.webp';
+
+export default function Features() {
+    return(
+        <div className= 'flex flex-col items-center text-black font-medium text-4xl m-16'>
+            <h1 className='font-black text-7xl text-main'>Features</h1>
+            
+            <div className='flex space-x-5 my-6 w-5/6 rounded-2xl shadow-lg p-8 bg-white'>
+                <div className='mx-12 p-48 bg-main_eighty text-white'>Pic/gif here</div>
+                <div>
+                    <h2>View your logs in the Sherlogs dashboard</h2>
+                    <p className='font-normal text-3xl pt-4 text-gray'>See a history of your logs complete with the date and time at which the log was created, the log level, and the log message.</p>
+                </div>
+            </div>
+            
+            <div className='flex space-x-5 my-6 w-5/6 rounded-2xl shadow-lg p-8 bg-white'>
+                <div className='mx-12 p-48 bg-main_eighty text-white'>Pic/gif here</div>
+                <div>
+                    <h2>Filter logs by time and level</h2>
+                    <p className='font-normal text-3xl pt-4 text-gray'>View logs from a chosen time period such as the last hour or the last week.  Display logs of a specific level such as "error" or "info".  Mix and match filters to refine your search.</p>
+                </div>
+            </div>
+
+            <div className='flex space-x-5 my-6 w-5/6 rounded-2xl shadow-lg p-8 bg-white'>
+                <div className='mx-12 p-48 bg-main_eighty text-white'>Pic/gif here</div>
+                <div>
+                    <h2>Search through logs for key words</h2>
+                    <p className='font-normal text-3xl pt-4 text-gray'>View logs that contain a specific word or phrase of interest.</p>
+                </div>
+            </div>
+
+            <div className='flex space-x-5 my-6 w-5/6 rounded-2xl shadow-lg p-8 bg-white'>
+                <div className='mx-12 p-48 bg-main_eighty text-white'>Pic/gif here</div>
+                <div>
+                    <h2>Collect logs using our custom logger</h2>
+                    <p className='font-normal text-3xl pt-4 text-gray'>Add our custom logger to any Node.js application to quickly and easily send logs to Sherlogs.  Customize the log levels and messages you wish to receive.</p>
+                    <p>Check out Quick Logger BE here!</p>
+                </div>
+            </div>
+
+            <div className='flex space-x-5 my-6 w-5/6 rounded-2xl shadow-lg p-8 bg-white'>
+                <img className='mx-12 w-1/2' src={dockerLogo} alt='docker-logo'></img>
+                <div>
+                    <h2>Run on any computer</h2>
+                    <p className='font-normal text-3xl pt-4 text-gray'>Sherlogs is containerized with Docker to bring developers the same experience, regardless of your operating system.</p>
+                </div>
+            </div>
+      </div>
+      
+    )
+}
--- gatsby/src/components/team.js
+++ gatsby/src/components/team.js
@@ -6,26 +6,30 @@ import felicity from '../assets/profile_pic_fn.jpeg'
 
 export default function Team() {
     return(
-        <div className='flex'>
-            <div className='flex flex-col items-center mx-12'>
-                <img className='rounded-full w-48 my-2' src={andres}></img>
-                <h3 className='text-white text-3xl font-medium'>Andrés Gutiérrez</h3>
-                <a className='text-white text-2xl' href='https://github.com/Agfeynman'>@Agfeynman</a>
-            </div>
-            <div className='flex flex-col items-center mx-12'>
-                <img className='rounded-full w-48 my-2' src={kylene}></img>
-                <h3 className='text-white text-3xl font-medium'>Kylene Hohman</h3>
-                <a className='text-white text-2xl' href='https://github.com/velveteen-ant'>@velveteen-ant</a>
-            </div>
-            <div className='flex flex-col items-center mx-12'>
-                <img className='rounded-full w-48 my-2' src={felicity}></img>
-                <h3 className='text-white text-3xl font-medium'>Felicity Nguyen</h3>
-                <a className='text-white text-2xl' href='https://github.com/felisity09'>@felisity09</a>
-            </div>
-            <div className='flex flex-col items-center mx-12'>
-                <img className='rounded-full w-48 my-2' src={crystal}></img>
-                <h3 className='text-white text-3xl font-medium'>Crystal Pederson</h3>
-                <a className='text-white text-2xl' href='https://github.com/crystalpederson'>@crystalpederson</a>
+        <div className='flex flex-col items-center bg-gradient-to-t from-main to-main_fifty p-16'>
+            <h1 className='text-white text-5xl m-2 mb-8 font-source_sans font-black'>Meet the Sherlogs Team</h1>
+            
+            <div className='flex flex-wrap'>
+                <div className='flex flex-col items-center mx-12'>
+                    <img className='rounded-full w-48 my-2' src={andres} alt='profile photo'></img>
+                    <h3 className='text-white text-3xl font-medium'>Andrés Gutiérrez</h3>
+                    <a className='text-white text-2xl' href='https://github.com/Agfeynman'>@Agfeynman</a>
+                </div>
+                <div className='flex flex-col items-center mx-12'>
+                    <img className='rounded-full w-48 my-2' src={kylene} alt='profile photo'></img>
+                    <h3 className='text-white text-3xl font-medium'>Kylene Hohman</h3>
+                    <a className='text-white text-2xl' href='https://github.com/velveteen-ant'>@velveteen-ant</a>
+                </div>
+                <div className='flex flex-col items-center mx-12'>
+                    <img className='rounded-full w-48 my-2' src={felicity} alt='profile photo'></img>
+                    <h3 className='text-white text-3xl font-medium'>Felicity Nguyen</h3>
+                    <a className='text-white text-2xl' href='https://github.com/felisity09'>@felisity09</a>
+                </div>
+                <div className='flex flex-col items-center mx-12'>
+                    <img className='rounded-full w-48 my-2' src={crystal} alt='profile photo'></img>
+                    <h3 className='text-white text-3xl font-medium'>Crystal Pederson</h3>
+                    <a className='text-white text-2xl' href='https://github.com/crystalpederson'>@crystalpederson</a>
+                </div>
             </div>
         </div>
     )
