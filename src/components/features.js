3d5c1724ab056d82ab99406a9aaced121f45429a
Crystal Pederson <crystalpederson88@gmail.com>
2022-04-29 20:52:04 -0700

    add images and gifs for features

--- src/components/features.js
+++ src/components/features.js
@@ -1,5 +1,9 @@
 import React from "react";
 import dockerLogo from '../assets/docker-logo.png';
+import displayLogs from '../assets/display-logs.gif';
+import filter from '../assets/filter.gif';
+import search from '../assets/search.gif';
+import quickLogger from '../assets/quick-logger.png';
 
 export default function Features() {
     return(
@@ -7,15 +11,15 @@ export default function Features() {
             <h1 className='font-black text-5xl text-main'>Features</h1>
             
             <div className='flex space-x-5 my-6 w-5/6 rounded-2xl shadow-lg p-8 bg-white'>
-                <div className='mx-12 p-48 bg-main_eighty text-white'>Pic/gif here</div>
-                <div>
+            <img className='px-4 w-2/3' src={displayLogs} alt='log display example'></img>
+                <div className='pl-4'>
                     <h2>View your logs in the Sherlogs dashboard</h2>
                     <p className='font-normal text-xl pt-4 text-gray'>See a history of your logs complete with the date and time at which the log was created, the log level, and the log message.</p>
                 </div>
             </div>
             
             <div className='flex space-x-5 my-6 w-5/6 rounded-2xl shadow-lg p-8 bg-white'>
-                <div className='mx-12 p-48 bg-main_eighty text-white'>Pic/gif here</div>
+                <img className='px-4 w-2/3' src={filter} alt='filter example'></img>
                 <div>
                     <h2>Filter logs by time and level</h2>
                     <p className='font-normal text-xl pt-4 text-gray'>View logs from a chosen time period such as the last hour or the last week.  Display logs of a specific level such as "error" or "info".  Mix and match filters to refine your search.</p>
@@ -23,7 +27,7 @@ export default function Features() {
             </div>
 
             <div className='flex space-x-5 my-6 w-5/6 rounded-2xl shadow-lg p-8 bg-white'>
-                <div className='mx-12 p-48 bg-main_eighty text-white'>Pic/gif here</div>
+            <img className='px-4 w-2/3' src={search} alt='search example'></img>
                 <div>
                     <h2>Search through logs for key words</h2>
                     <p className='font-normal text-xl pt-4 text-gray'>View logs that contain a specific word or phrase of interest.</p>
@@ -31,16 +35,16 @@ export default function Features() {
             </div>
 
             <div className='flex space-x-5 my-6 w-5/6 rounded-2xl shadow-lg p-8 bg-white'>
-                <div className='mx-12 p-48 bg-main_eighty text-white'>Pic/gif here</div>
+                <img className='px-4 w-2/3' src={quickLogger} alt='quick logger example'></img>
                 <div>
                     <h2>Collect logs using our custom logger</h2>
                     <p className='font-normal text-xl pt-4 text-gray'>Add our custom logger to any Node.js application to quickly and easily send logs to Sherlogs.  Customize the log levels and messages you wish to receive.</p>
-                    <p className='font-normal text-xl pt-4 text-gray'>Check out Quick Logger BE here!</p>
+                    <p className='font-normal text-xl pt-4 text-gray'>Check out Quick Logger BE <a href='https://www.quickloggerbe.com/' target='_blank' rel='noopener noreferrer' className='text-main font-semibold'>HERE</a>!</p>
                 </div>
             </div>
 
             <div className='flex space-x-5 my-6 w-5/6 rounded-2xl shadow-lg p-8 bg-white'>
-                <img className=' px-12 w-1/2 border-r-4 border-ltGray' src={dockerLogo} alt='docker-logo'></img>
+                <img className=' px-12 w-1/2' src={dockerLogo} alt='docker-logo'></img>
                 <div>
                     <h2>Run on any computer</h2>
                     <p className='font-normal text-xl pt-4 text-gray'>Sherlogs is containerized with Docker to bring developers the same experience, regardless of your operating system.</p>
