11207d4247bcaa526976b5cf77afdc67570764d3
Crystal Pederson <crystalpederson88@gmail.com>
2022-04-29 21:43:49 -0700

    finish styling buttons

--- src/components/features.js
+++ src/components/features.js
@@ -7,7 +7,7 @@ import quickLogger from '../assets/quick-logger.png';
 
 export default function Features() {
     return(
-        <div className= 'flex flex-col items-center text-black font-semibold text-2xl m-16'>
+        <div className= 'flex flex-col items-center text-black font-semibold text-3xl m-16'>
             <h1 className='font-black text-5xl text-main'>Features</h1>
             
             <div className='flex space-x-5 my-6 w-5/6 rounded-2xl shadow-lg p-8 bg-white'>
@@ -38,8 +38,8 @@ export default function Features() {
                 <img className='px-4 w-[400px]' src={quickLogger} alt='quick logger example'></img>
                 <div>
                     <h2>Collect logs using our custom logger</h2>
-                    <p className='font-normal text-xl pt-4 text-gray'>Add our custom logger to any Node.js application to quickly and easily send logs to Sherlogs.  Customize the log levels and messages you wish to receive.</p>
-                    <p className='font-normal text-xl pt-4 text-gray'>Check out Quick Logger BE <a href='https://www.quickloggerbe.com/' target='_blank' rel='noopener noreferrer' className='text-main font-semibold'>HERE</a>!</p>
+                    <p className='font-normal text-xl pt-4 pb-4 text-gray'>Add our custom logger to any Node.js application to quickly and easily send logs to Sherlogs.  Customize the log levels and messages you wish to receive.</p>
+                    <a className='p-2 bg-main rounded-lg drop-shadow-lg text-white text-xl font-semibold hover:bg-white hover:text-main hover:border-2' href='https://www.quickloggerbe.com/' target='_blank' rel='noopener noreferrer'>Check out Quick Logger BE</a>
                 </div>
             </div>
 
